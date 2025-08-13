3a0197cb1cff1c2fc63b7bf28da60c98181ff480
hjc2000 <hjc.666@foxmail.com>
2025-08-13 16:12:32 +0800

    重构测试程序配置并添加新的测试主程序

--- project.cmake
+++ project.cmake
@@ -8,12 +8,12 @@ target_import_ffmpeg_wrapper(${ProjectName} PUBLIC)
 # 添加测试程序
 if(("${platform}" STREQUAL "msys") OR
    ("${platform}" STREQUAL "msys-clang"))
-	set(exe_name "test")
-	add_executable(${exe_name} ${CMAKE_CURRENT_SOURCE_DIR}/exe/main.cpp)
+	set(test_exe_target_name "test")
+	add_executable(${test_exe_target_name})
+	target_import_test(${test_exe_target_name})
 
 	# 将资源目录用预定义传给 C++ 程序
-	target_compile_definitions(${exe_name} PRIVATE Predefine_ResourceDir="D:/video/视频开发测试")
+	target_compile_definitions(${test_exe_target_name} PRIVATE Predefine_ResourceDir="D:/video/视频开发测试")
 
-	target_link_libraries(${exe_name} ${ProjectName})
-	target_install(${exe_name})
+	target_link_libraries(${test_exe_target_name} ${ProjectName})
 endif()
