3cdb60a02db8e4e7c0270f6c7473ef4182939753
hjc2000 <hjc.666@foxmail.com>
2025-08-13 16:15:08 +0800

    将测试程序的编译定义和链接库可见性改为PUBLIC

--- project.cmake
+++ project.cmake
@@ -13,7 +13,7 @@ if(("${platform}" STREQUAL "msys") OR
 	target_import_test(${test_exe_target_name})
 
 	# 将资源目录用预定义传给 C++ 程序
-	target_compile_definitions(${test_exe_target_name} PRIVATE Predefine_ResourceDir="D:/video/视频开发测试")
+	target_compile_definitions(${test_exe_target_name} PUBLIC Predefine_ResourceDir="D:/video/视频开发测试")
 
-	target_link_libraries(${test_exe_target_name} ${ProjectName})
+	target_link_libraries(${test_exe_target_name} PUBLIC ${ProjectName})
 endif()
